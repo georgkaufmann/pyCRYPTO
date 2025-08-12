f07674f17cc2fa2a48a790553030045f9748450b
Georg Kaufmann <georg.kaufmann@fu-berlin.de>
2025-08-12 09:43:24 +0200

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<table>
+<tr><td><img style="height: 150px;" src="images/geo_hydro1.jpg"></td>
+<td bgcolor="#CDCDCD" style="width:600px">
+<h1>pyCRYPTO</h1>
+<h2>CRYPTOgraphy</h2>
+<h2>Georg Kaufmann</h2>
+</td>
+</tr>
+</table>
+
+----
+
+# Geodynamical models
+
+This repository is part of a series of repositories on 
+numerical modelling in geosciences, see my
+[github page](https://github.com/georgkaufmann).
+
+----
+
+# **pyCRYPTO**
+
+We explore and describe some simple cryptographic tools and models.
+The classisal RSA encryption will be covered,
+and a blockchain is coded.
+
+- A technical description can be found in the [github repository](https://github.com/georgkaufmann/pyIMAGE).
+- A discussion of modelling results is given in the [(Dynamics)<sup>2</sup> blog](https://blog.kawueweb.de/dynamics).
---
+++ libCrypt.py
@@ -0,0 +1,197 @@
+"""
+functions for prime number generation, coprime testing, ...
+"""
+
+def findPrimesSimple(upper=100):
+    """
+    pyCRYPTO
+    create list of prime numbers explicitely
+    using the modulo operator
+    """
+    # set array of prime numbers to empty array
+    primes=[]
+    # loop over potential prime numbers
+    for n in range(2,upper):
+        # inner loop to check division
+        isprime=True
+        for i in range(2,n):
+            if ((n % i) == 0):
+                isprime=False
+               #break
+        if (isprime): primes.append(n)
+    return primes
+
+
+def isPrimeFromList(n):
+    """
+    pyCRYPTO
+    check if number is prime number 
+    by comparing to a list
+    """
+    # create list of prime numbers
+    primeList = findPrimes_simple(1000)
+    # check is number is larger than larest element in list
+    if (n > primeList[-1]):
+        sys.exit('Error: number larger than maximum in tabulated primes')
+    # check if number is in list
+    if n in primeList:
+        return True
+    return False
+
+
+def isPrimeRabinMiller(n):
+    """
+    pyCRYPTO
+    uses Rabin-Miller algorithm to estimate if n is a prime
+    """
+    import random
+    # check if number is even, then break
+    if n % 2 == 0: return False
+    
+    s = n - 1
+    t = 0
+    while s % 2 == 0:
+        # keep halving s while it is even (and use t
+        # to count how many times we halve s)
+        s = s // 2
+        t += 1
+    for trials in range(5): # try to falsify num's primality 5 times
+        a = random.randrange(2,n-1)
+        v = pow(a, s, n)
+        if v != 1: # this test does not apply if v is 1.
+            i = 0
+            while v != (n-1):
+                if i == t-1:
+                    return False
+                else:
+                    i += 1
+                    v = (v**2) % n
+    return True
+
+
+def generateLargePrime(keysize=1024):
+    """
+    pyCRYPTO
+    Return a random prime number of keysize bits in size
+    """
+    import random
+    while True:
+        number = random.randrange(2**(keysize-1), 2**(keysize))
+        if isPrimeRabinMiller(number):
+            return number
+
+
+def primeFactors(n):
+    """
+    pyCRYPTO
+    Determine all prime factors of a composite number
+    input:
+      n - integer number
+    output:
+      factors - list of prime factors
+    """
+    factors = []
+    # first check how many 2 are in as prime factors
+    while (n%2 == 0):
+        factors.append(2)
+        n = n/2
+    # then, n is returned as odd number, and next we check other prime factors
+    for i in range(3,int(np.sqrt(n))+1,2):
+        while (n%i == 0):
+            factors.append(i)
+            n = n/i
+    # remaining factor ...       
+    if (n > 2):
+        factors.append(int(n))
+    return factors
+
+
+def greatestCommonDivisor(a,b):
+    """
+    pyCRYPTO
+    Determine greatest common divisor for two numbers a,b
+    input:
+      a,b - integer numbers
+    output:
+      GCA - greatest common divisor
+    """
+    r = a%b
+    while r:
+        a = b
+        b = r
+        r = a%b
+    return b
+
+
+def RSAcreateKeys(keySize=1024):
+    """
+    pyCRYPTO
+    create public-private key sequence for RSA crypting
+    input:
+      keySize - key size 
+    output:
+      publicKey  - public key tuple
+      privateKey - private key tuple
+    """
+    import random
+    import libCrypt
+    # generate two large prime
+    p = libCrypt.generateLargePrime(keySize)
+    q = libCrypt.generateLargePrime(keySize)
+    # calculate product of primes
+    n = p*q
+    # calculate phi-function
+    phi=(p-1)*(q-1)
+    # search number e, which is coprime to phi
+    while True:
+        # Keep trying random numbers for e until one is valid.
+        e = random.randrange(2**(keySize-1),2**(keySize))
+        if libCrypt.greatestCommonDivisor(e, (p-1)*(q-1)) == 1:
+            break
+    # calculate d as modular inverse e mod phi
+    d = pow(e,-1,phi)    
+    #print(p,q,n,e,d)
+    # save askey pair
+    publicKey = (keySize,n,e)
+    privateKey = (keySize,n,d)
+    return publicKey,privateKey
+
+
+def RSAencrypt(message,publicKey,debug=False):
+    """
+    pyCRYPTO
+    RSA encryption of a string, using the locale
+    input:
+      message - message text
+      publicKey - public key tuple (keylength,N,E)
+    output:
+      encrypt - encrypted message
+    """
+    keySize,n,e = publicKey
+    messageBytes = message.encode()
+    encrypt = []
+    for byte in messageBytes:
+        # encrypt with public key
+        encrypt.append(pow(byte,e,n))
+    if (debug):
+        print(encrypt)
+    return encrypt
+
+
+def RSAdecrypt(encrypted_message,privateKey,debug=False):
+    """
+    pyCRYPTO
+    RSA encryption of a string, using the locale
+    input:
+      encrypted_message - encrypted message text
+      privateKey - private key tuple (keylength,N,E)
+    output:
+      decrypted_message - decrypted message
+    """
+    keySize,n,d = privateKey
+    decrypt = []
+    for byte in encrypted_message:
+        decrypt.append(pow(byte,d,n))
+    if (debug):
+        print(decrypt)
+    return bytearray(decrypt).decode()
